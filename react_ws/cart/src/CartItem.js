41a000334a34f0242f47e4af1b4b78052ae41e7d
divya-ninja <divya140294@gmail.com>
2021-07-21 17:33:01 +0530

    setState in depth

--- react_ws/cart/src/CartItem.js
+++ react_ws/cart/src/CartItem.js
@@ -1,18 +1,95 @@
 import React from 'react';
 
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            title: 'Phone',
+            price: 9999,
+            qty: 1,
+            img: ''
+        }
+        // this.increaseQuantity = this.increaseQuantity.bind(this);
+        this.testing();
+    }
+
+    testing(){
+        const promise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve('done');
+            }, 5000);
+        })
+
+        promise.then(() => {
+            // setState acts like a synchronous call
+            this.setState({qty: 100});
+            console.log('state', this.state);
+        })
+
+    }
+
+    increaseQuantity = () => {
+        //this.state.qty += 1;
+        //console.log('this', this.state);
+        // setState form 1
+        // this.setState({
+        //     qty: this.state.qty + 1
+        // });
+        
+        // setState form 2 - use this if previous state is required
+        this.setState((prevState) => {
+            return{
+                qty: prevState.qty + 1
+            }
+        }, () => {
+            console.log('state', this.state);
+        });
+    }
+
+    decreaseQuantity = () => {
+        const {qty} = this.state;
+
+        if(qty === 0){
+            return;
+        }
+
+        this.setState((prevState) => {
+            return {
+                qty: prevState.qty - 1
+            }
+        });
+    }
+
     render(){
+        const {title, price, qty} = this.state;
         return(
             <div className = "cart-item">
-                <div classsName = "left-block">
-                    <img style = {styles.image}/>
+                <div className = "left-block">
+                    <img alt = "" style = {styles.image}/>
                 </div>
                 <div className = "right-block">
-                    <div style = {{fontSize: 25}}>phone</div>
-                    <div style = {{color: 'grey'}}>Rs 9999</div>
-                    <div style = {{color: 'grey'}}>Qty : 1</div>
+                    <div style = {{fontSize: 25}}> {title} </div>
+                    <div style = {{color: 'grey'}}>Rs {price}</div>
+                    <div style = {{color: 'grey'}}>Qty : {qty}</div>
                     <div className = "cart-item-actions">
                         {/* buttons */}
+                        <img 
+                            alt = "increase" 
+                            className = "action-icons" 
+                            src = "https://image.flaticon.com/icons/png/512/992/992651.png"
+                            onClick = {this.increaseQuantity}
+                        />
+                        <img 
+                            alt = "decrease" 
+                            className = "action-icons" 
+                            src = "https://image.flaticon.com/icons/png/512/992/992683.png"
+                            onClick = {this.decreaseQuantity}
+                        />
+                        <img 
+                            alt = "delete" 
+                            className = "action-icons" 
+                            src = "https://image.flaticon.com/icons/png/512/1214/1214428.png"
+                        />
                     </div>
                 </div>
             </div>
